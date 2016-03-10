9e5970358c101a5ecbd13b338aaa858eb59cc971
Steve <stevenlyall@gmail.com>
2016-03-10 15:34:03 -0800

    dates shown

--- app/views/view-results/view-results.js
+++ app/views/view-results/view-results.js
@@ -14,19 +14,58 @@ angular.module('app.view-results', ['ngRoute'])
     // TODO handle page access if evaluation status isn't finished
 
     var evalId = $routeParams.evalId;
-    $http
-      .get('http:' + envService.read('apiUrl') + '/evaluations/' + evalId, {
-        headers: authService.getAPITokenHeader()
-      }).then(success, fail);
+    getEvaluation(evalId);
+    getResultsForEvaluation(evalId);
 
-    function success(response) {
-      $scope.evaluation = response.data[0];
-      $scope.evaluation.resultsAvailableDate = new Date($scope.evaluation.resultsAvailableDate);
+    function getEvaluation(id) {
+      $http
+        .get('http:' + envService.read('apiUrl') + '/evaluations/' + id, {
+          headers: authService.getAPITokenHeader()
+        }).then(success, fail);
+
+      function success(response) {
+        console.log('retrieved eval');
+        console.log(response);
+        $scope.evaluation = response.data[0];
+      }
+
+      function fail(response) {
+        console.error(response.data);
+      }
+
     }
 
-    function fail(response) {
-      console.error(response.data);
+    function getResultsForEvaluation(id) {
+      $http
+        .get('http:' + envService.read('apiUrl') + '/evaluations/' + evalId + '/results', {
+          headers: authService.getAPITokenHeader()
+        }).then(success, fail);
+
+      function success(response) {
+        $scope.results = response.data;
+        $scope.results.responsesStartDate = getAndFormatDateTime($scope.results.responsesStartDate);
+        $scope.results.responsesEndDate = getAndFormatDateTime($scope.results.responsesEndDate);
+
+        console.log($scope.results);
+      }
+
+      function fail(response) {
+        console.error(response.data);
+      }
+
     }
 
+    //// TODO move to another module where can be shared
+    function getAndFormatDateTime(date) {
+      var result = new Date(date);
+      result = result.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+      });
+      return result;
+    }
 
   }]);
