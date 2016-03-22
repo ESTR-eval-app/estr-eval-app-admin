d3f5b6f05f51e51bfd620a704016a04a11212543
Steve <stevenlyall@gmail.com>
2016-03-21 17:47:04 -0700

    both pdf downloads working

--- app/views/view-results/view-results.js
+++ app/views/view-results/view-results.js
@@ -73,9 +73,7 @@ angular.module('app.view-results', ['ngRoute'])
       return result;
     }
 
-    // todo if NA or DK resps allowed, append to labels array
-    // TODO move this in case needs to be changed
-
+    // todo account for NA or DK responses and scale, currently ignored
     function getChartData() {
       $scope.type = "HorizontalBar";
       $scope.chartData = [];
@@ -105,9 +103,7 @@ angular.module('app.view-results', ['ngRoute'])
       html += "<p> Response data collected between <b>" + $scope.results.responsesStartDate +
         "</b> and <b>" + $scope.results.responsesEndDate + "</b></p><br><br>";
       html += "<p># of completed evaluations received: <b>" + $scope.results.numResponses + "</b></p><br><br>";
-      html += "<p>Number of quantitative questions in evaluation: <b>" + $scope.results.responseCounts.length + "</b></p>";
 
-      console.log(html);
       pdf.fromHTML(html, 15, 15, options, function (dispose) {
         createGraphPages();
       });
@@ -126,35 +122,56 @@ angular.module('app.view-results', ['ngRoute'])
           var html = "<h2>Question Responses</h2>";
           html += '<h2>"' + questions[i].text + '"</h2>';
 
-          //console.log(html)
-          // Todo get image
           var e = document.getElementById('bar' + i);
           var imgData = e.toDataURL('image/png');
 
           html += "<img height='80' src='" + imgData + "' >";
-          //console.log(html)
           pdf.fromHTML(html, 15, 15, options, function (dispose) {
             console.log('saved');
             pagesDone++;
             if (pagesDone == 5) {
-              saveReport();
+              saveReport(pdf, "Graphs");
             }
           });
           pdf.addPage();
         }
-
-        function saveReport() {
-          var reportName = $scope.evaluation.name;
-          reportName = reportName.replace(/ /g, "_");
-          pdf.save("Result_Graphs_" + reportName + ".pdf");
-        }
-
       }
-
-      $scope.downloadCommentsBtnClick = function () {
-        // TODO
-      }
-
     };
+
+    $scope.downloadCommentsBtnClick = function () {
+      var pdf = new jsPDF();
+      var options = {width: 170};
+      var html = "<h1> Eval <sup>n</sup></h1>";
+      html += "<h1>Qualitative Response Report</h1><br><br><br>";
+      html += "<p>Generated on <b>" + getAndFormatDateTime(new Date()) + "</b><p><br><br><br>";
+      html += "<br><br><br>";
+      html += "<p>";
+      if ($scope.evaluation.isAnonymous) {
+        html += "Anonymous responses";
+      }
+      else {
+        html += "Responses";
+      }
+      html += " collected between <b>" + $scope.results.responsesStartDate +
+        "</b> and <b>" + $scope.results.responsesEndDate + "</b></p><br><br>";
+      html += "<p># of completed evaluations received: <b>" + $scope.results.numResponses + "</b></p><br><br>";
+      pdf.fromHTML(html, 15, 15, options, function (dispose) {
+        pdf.addPage();
+        addCommentsToReport();
+      });
+
+      function addCommentsToReport() {
+        pdf.fromHTML($("#comments").get(0), 15, 15, options, function (dispose) {
+          saveReport(pdf, "Comments");
+        });
+      }
+    };
+
+    function saveReport(pdf, reportType) {
+      var reportName = $scope.evaluation.name;
+      reportName = reportName.replace(/ /g, "_");
+      pdf.save("Report_" + reportType + "_" + reportName + ".pdf");
+    }
+
 
   }]);
